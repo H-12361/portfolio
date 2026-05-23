beec83937588cf7168248088c45f17bd4989f6b6
H-12361 <harshittiwariharshittiwari8@gmail.com>
2026-05-23 10:28:30 +0530

    bervo used

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -2,15 +2,12 @@ import nodemailer from "nodemailer";
 
 const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
+    host: "smtp-relay.brevo.com",
+    port: 587,
+    secure: false,
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-    tls: {
-      rejectUnauthorized: false,
+      user: process.env.BREVO_USER,
+      pass: process.env.BREVO_PASS,
     },
   });
 
